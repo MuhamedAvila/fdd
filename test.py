07bcbd715fe6d72588f72e3d07d6feb527ed3bd6
muhamed <muhamed@cpqd.com.br>
2017-08-11 11:13:57 -0300

    New file added (test.py)

---
+++ test.py
@@ -0,0 +1,12 @@
+def mult(x, y):
+    return x * y
+
+def sum(x, y):
+    return x + y
+
+def subtratction(x, y):
+    return x - y
+
+print "the sum is:", sum(4, 5)
+print "the multiplication is:", mult(4,5)
+print "the subtraction is:", mult(4,5)
